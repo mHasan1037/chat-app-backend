a0f476881d98f5e9674321e5867a3ab516864c46
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2025-12-15 16:21:48 +0600

    Made the profile route controllers

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import { protectedRoute } from '../middleware/authMiddleware';
-import { searchUsers } from '../controllers/userController';
+import { getUserProfile, searchUsers, updateMyProfile, unfriendUser } from '../controllers/userController';
 
 const router = express.Router();
 
 router.get('/search', protectedRoute, searchUsers);
+router.patch('/me', protectedRoute, updateMyProfile);
+router.delete('/friends/:id', protectedRoute, unfriendUser);
+router.get('/:id', protectedRoute, getUserProfile);
 
 export default router;
