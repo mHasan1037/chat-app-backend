30aeee05e49a1e38e5ad087e1a8095514ef9b824
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2026-01-20 17:19:33 +0600

    Added the audio call feature

--- utils/socket.ts
+++ utils/socket.ts
@@ -29,7 +29,38 @@ export const initSocket = (server: HTTPServer) =>{
 
         socket.on("joinConversation", (conversationId: string) => {
             socket.join(conversationId);
-        })
+        });
+
+        socket.on("call:offer", ({toUserId, fromUserId, conversationId, offer}) =>{
+            io?.to(`user:${toUserId}`).emit("call:offer", {
+                fromUserId, 
+                conversationId,
+                offer
+            })
+        });
+
+        socket.on("call:answer", ({ toUserId, fromUserId, conversationId, answer }) =>{
+           io?.to(`user:${toUserId}`).emit("call:answer", {
+             fromUserId,
+             conversationId,
+             answer
+           })
+        });
+
+        socket.on("call:ice", ({toUserId, fromUserId, conversationId, candidate}) =>{
+            io?.to(`user:${toUserId}`).emit("call:ice", {
+                fromUserId,
+                conversationId,
+                candidate
+            })
+        });
+
+        socket.on("call:end", ({toUserId, fromUserId, conversationId}) =>{
+            io?.to(`user:${toUserId}`).emit('call:end', {
+                fromUserId,
+                conversationId 
+            })
+        });
     });
 
     return io;
