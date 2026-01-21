3cb2575de05bb127f4a3a82ecb8cc838f8c439ae
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2026-01-21 18:48:27 +0600

    cors updated

--- app.ts
+++ app.ts
@@ -1,29 +1,32 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './config/db';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db";
 
 import authRoutes from "./routes/authRoutes";
 import friendRequestRoutes from "./routes/friendRequestRoutes";
 import userRoutes from "./routes/userRoutes";
-import chatRoutes from './routes/chatRoutes';
-import messageRoutes from './routes/messageRoutes';
-
+import chatRoutes from "./routes/chatRoutes";
+import messageRoutes from "./routes/messageRoutes";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 connectDB();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/friends", friendRequestRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/chats', chatRoutes);
-app.use('/api/messages', messageRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/chats", chatRoutes);
+app.use("/api/messages", messageRoutes);
 
 export default app;
-
